152a8e81bb00aa835fa641e4c56489670ac1e74e
Tirkirill <sherbakirill@mail.ru>
2021-01-30 13:26:56 +0300

    Перехват chrome.runtime.lastError

--- popup.js
+++ popup.js
@@ -3,13 +3,27 @@ if (create_file_button) {
 	create_file_button.onclick = () => {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var tab = tabs[0];
-			alert(tab.url, tab.title);
-			chrome.tabs.getSelected(null, function(tab) {
-				alert(tab.id);
-				chrome.tabs.sendMessage(tab.id, {greeting: "hello"}, function(res) {
-					alert('onResponse' + res.isTestTab);
+			try {
+				chrome.tabs.getSelected(null, function(tab) {
+					chrome.tabs.sendMessage(tab.id, {}, function(res) {
+						if (chrome.runtime.lastError) {
+							alert("Попробуйте перезагрузить страницу!");
+						}
+						else {
+							alert(res.isTestTab);
+						}
+					});
 				});
-			});
+			}
+			catch(e){
+				if (e instanceof runtime.lastError) {
+					alert("Попробуйте перезагрузить страницу!")
+				}
+				else {
+					alert("Непридвиденная ошибка!");
+					alert(e);
+				}
+			}
 		});
 	}
 }
