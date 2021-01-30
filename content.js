362a46dc939d33c258b6a2761fb1158ead815666
Tirkirill <sherbakirill@mail.ru>
2021-01-30 15:36:56 +0300

    Загрузка отчета для решения теста

--- content.js
+++ content.js
@@ -1,94 +1,108 @@
 window.onload = function() {
-    chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
-        var res = {};
-		let isTest = document.querySelector(".quiz-content");
-		if (isTest) {
-			res["isTestTab"] = true;
-			res["questions"] = [];
-			let questions = document.querySelectorAll(".question");
-			for (let i=0; i<questions.length; i++) {
-				let question = questions[i];
-				let hint = question.querySelector(".hint");
-				let type = "";
-				let isCorrect = question.classList.contains("correct");
-				switch (hint.textContent) {
-					case "Выберите один правильный ответ":
-						type="radio";
-						break;
-					case "Введите ответ в виде текста (регистр не учитывается)":
-					case "Введите на месте пропуска текст (регистр не учитывается)":
-						type = "text";
-						break;
-					case "Соедините элементы попарно (неверно соединенную пару можно разбить, щелкнув на крестик)":
-						type = "connectPairs";
-						break;
-					case "Выберите один или несколько правильных ответов":
-						type="checks";
-						break;
-					case "Выберите из выпадающего списка правильный ответ":
-						type="select";
-						break;
-					case "Расставьте в правильном порядке":
-						type="order";
-						break;
-				}
-				res["questions"][i] = {};
-				res["questions"][i]["title"] = question.querySelector(".question-description p").textContent;
-				res["questions"][i]["type"] = type;
-				res["questions"][i]["isCorrect"] = isCorrect;
-				let answer;
-				switch (type) {
-					case "radio":
-						let choosenLabel = question.querySelector("label.checked");
-						let choosenRadioOuterHtml = choosenLabel.querySelector("input").outerHTML;
-						answer = choosenLabel.innerHTML.replace(choosenRadioOuterHtml, "").trim();
-						break;
-					case "text":
-						let response = question.querySelector(".response input");
-						answer = response.value;
-						break;
-					case "connectPairs":
-						answer = {};
-						let pairs = question.querySelectorAll(".qt_connect_group");
-						for (pair of pairs) {
-							let items = pair.querySelectorAll(".qt_connect_item");
-							answer[items[0].innerHTML.trim()] = items[1].innerHTML.trim();
-						}
-						break;
-					case "checks":
-						answer = [];
-						let labels = question.querySelectorAll("label");
-						for (let label of labels) {
-							let box = label.querySelector("input[type='checkbox']");
-							if (box.checked) {
-								answer.push(label.innerHTML.replace(box.outerHTML, "").trim());
-							}
-						}
-						break;
-					case "select":
-						let select = question.querySelector("select");
-						answer = select.querySelector("option[selected='selected'").textContent;
-						break;
-					case "order":
-						answer = [];
-						let questionOrder = question.querySelector(".question-order");
-						let items = questionOrder.querySelectorAll("div");
-						for (let item of items) {
-							answer.push(item.textContent);
-						}
-						break;
-					case "":
-						console.log("Такой тип вопроса не учтен...\nВопрос: " + res["questions"][i]["title"] + "\nHint: "+hint.textContent);
-						break;
-				}
-				console.log(answer!=undefined);
-				res["questions"][i]["answer"] = answer;
-			}
+    chrome.runtime.onMessage.addListener(function(req, _, sendResponse) {
+		if (req.action == "create") {
+			createFile(sendResponse);
 		}
-		else {
-            res["isTestTab"] = false;
+		if (req.action == "solve") {
+			solveTest(sendResponse, req.answerJSON);
 		}
-		console.log(res);
-        sendResponse(res);
-    });
+	});
 };
+
+function createFile(sendResponse) {
+	var res = {};
+	let isTest = document.querySelector(".quiz-content");
+	if (isTest) {
+		res["isTestTab"] = true;
+		res["questions"] = [];
+		let questions = document.querySelectorAll(".question");
+		for (let i=0; i<questions.length; i++) {
+			let question = questions[i];
+			let hint = question.querySelector(".hint");
+			let type = "";
+			let isCorrect = question.classList.contains("correct");
+			switch (hint.textContent) {
+				case "Выберите один правильный ответ":
+					type="radio";
+					break;
+				case "Введите ответ в виде текста (регистр не учитывается)":
+				case "Введите на месте пропуска текст (регистр не учитывается)":
+					type = "text";
+					break;
+				case "Соедините элементы попарно (неверно соединенную пару можно разбить, щелкнув на крестик)":
+					type = "connectPairs";
+					break;
+				case "Выберите один или несколько правильных ответов":
+					type="checks";
+					break;
+				case "Выберите из выпадающего списка правильный ответ":
+					type="select";
+					break;
+				case "Расставьте в правильном порядке":
+					type="order";
+					break;
+			}
+			res["questions"][i] = {};
+			res["questions"][i]["title"] = question.querySelector(".question-description p").textContent;
+			res["questions"][i]["type"] = type;
+			res["questions"][i]["isCorrect"] = isCorrect;
+			let answer;
+			switch (type) {
+				case "radio":
+					let choosenLabel = question.querySelector("label.checked");
+					let choosenRadioOuterHtml = choosenLabel.querySelector("input").outerHTML;
+					answer = choosenLabel.innerHTML.replace(choosenRadioOuterHtml, "").trim();
+					break;
+				case "text":
+					let response = question.querySelector(".response input");
+					answer = response.value;
+					break;
+				case "connectPairs":
+					answer = {};
+					let pairs = question.querySelectorAll(".qt_connect_group");
+					for (pair of pairs) {
+						let items = pair.querySelectorAll(".qt_connect_item");
+						answer[items[0].innerHTML.trim()] = items[1].innerHTML.trim();
+					}
+					break;
+				case "checks":
+					answer = [];
+					let labels = question.querySelectorAll("label");
+					for (let label of labels) {
+						let box = label.querySelector("input[type='checkbox']");
+						if (box.checked) {
+							answer.push(label.innerHTML.replace(box.outerHTML, "").trim());
+						}
+					}
+					break;
+				case "select":
+					let select = question.querySelector("select");
+					answer = select.querySelector("option[selected='selected'").textContent;
+					break;
+				case "order":
+					answer = [];
+					let questionOrder = question.querySelector(".question-order");
+					let items = questionOrder.querySelectorAll("div");
+					for (let item of items) {
+						answer.push(item.textContent);
+					}
+					break;
+				case "":
+					console.log("Такой тип вопроса не учтен...\nВопрос: " + res["questions"][i]["title"] + "\nHint: "+hint.textContent);
+					break;
+			}
+			console.log(answer!=undefined);
+			res["questions"][i]["answer"] = answer;
+		}
+	}
+	else {
+		res["isTestTab"] = false;
+	}
+	console.log(res);
+	sendResponse(res);
+}
+
+function solveTest(sendResponse, answerJSON) {
+	console.log(answerJSON);
+	sendResponse({});
+}
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
 var create_file_button = document.querySelector("#create_file__button");
+var solve_test_button = document.querySelector("#solve_test__button");
 if (create_file_button) {
 	create_file_button.onclick = () => {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			chrome.tabs.getSelected(null, function(tab) {
-				chrome.tabs.sendMessage(tab.id, {}, function(res) {
+				chrome.tabs.sendMessage(tab.id, {action:"create"}, function(res) {
 					if (chrome.runtime.lastError) {
 						alert("Попробуйте перезагрузить страницу!");
 					}
@@ -12,11 +13,31 @@ if (create_file_button) {
 						var hiddenElement = document.createElement('a');
 						hiddenElement.href = 'data:attachment/text,' + encodeURI(JSON.stringify(res));
 						hiddenElement.target = '_blank';
-						hiddenElement.download = 'Test.json';
+						hiddenElement.download = 'Test.txt';
 						hiddenElement.click();
 					}
 				});
 			});
 		});
+	}
+	solve_test_button.onclick = () => {
+		let file = document.querySelector("#answerJSON").files[0];
+		var reader = new FileReader();
+		reader.readAsText(file);
+		reader.onload = function() {
+			let text = reader.result;
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.tabs.getSelected(null, function(tab) {
+					chrome.tabs.sendMessage(tab.id, {action:"solve", answerJSON: text}, function(res) {
+						if (chrome.runtime.lastError) {
+							alert("Попробуйте перезагрузить страницу!");
+						}
+						else {
+
+						}
+					});
+				});
+			});
+		}
 	}
 }
