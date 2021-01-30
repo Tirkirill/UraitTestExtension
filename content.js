02f7bb20888b8dccf7563e457bacd2bbb13fcf37
Tirkirill <sherbakirill@mail.ru>
2021-01-30 14:32:05 +0300

    Скачивание результата в виде JSON файла

--- content.js
+++ content.js
@@ -3,15 +3,47 @@ window.onload = function() {
         var res = {};
 		let isTest = document.querySelector(".quiz-content");
 		if (isTest) {
-            res["isTestTab"] = true;
-			questions = document.querySelectorAll(".question");
-			for (let question of questions) {
-				console.log(question.querySelector(".question-description p").innerHTML);
+			res["isTestTab"] = true;
+			res["questions"] = [];
+			let questions = document.querySelectorAll(".question");
+			for (let i=0; i<questions.length; i++) {
+				let question = questions[i];
+				let hint = question.querySelector(".hint");
+				let type;
+				let isCorrect = question.classList.contains("correct");
+				if (hint.textContent=="Выберите один правильный ответ") type = "radio";
+				if (hint.textContent=="Введите ответ в виде текста (регистр не учитывается)") type = "text";
+				if (hint.textContent=="Соедините элементы попарно (неверно соединенную пару можно разбить, щелкнув на крестик)") type = "connectPairs";
+				res["questions"][i] = {};
+				res["questions"][i]["title"] = question.querySelector(".question-description p").innerHTML;
+				res["questions"][i]["type"] = type;
+				res["questions"][i]["isCorrect"] = isCorrect;
+				switch (type) {
+					case "radio":
+						let choosenLabel = question.querySelector("label.checked");
+						let choosenRadioOuterHtml = choosenLabel.querySelector("input").outerHTML;
+						res["questions"][i]["answer"] = choosenLabel.innerHTML.replace(choosenRadioOuterHtml, "").trim();
+						break;
+					case "text":
+						let response = question.querySelector(".response input");
+						res["questions"][i]["answer"] = response.value;
+						break;
+					case "connectPairs":
+						let pairs = question.querySelectorAll(".qt_connect_group");
+						let answer = {};
+						for (pair of pairs) {
+							let items = pair.querySelectorAll(".qt_connect_item");
+							answer[items[0].innerHTML.trim()] = items[1].innerHTML.trim();
+						}
+						res["questions"][i]["answer"] = answer;
+						break;
+				}
 			}
 		}
 		else {
             res["isTestTab"] = false;
 		}
+		console.log(res);
         sendResponse(res);
     });
 };
--- popup.js
+++ popup.js
@@ -2,28 +2,21 @@ var create_file_button = document.querySelector("#create_file__button");
 if (create_file_button) {
 	create_file_button.onclick = () => {
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			var tab = tabs[0];
-			try {
-				chrome.tabs.getSelected(null, function(tab) {
-					chrome.tabs.sendMessage(tab.id, {}, function(res) {
-						if (chrome.runtime.lastError) {
-							alert("Попробуйте перезагрузить страницу!");
-						}
-						else {
-							alert(res.isTestTab);
-						}
-					});
+			chrome.tabs.getSelected(null, function(tab) {
+				chrome.tabs.sendMessage(tab.id, {}, function(res) {
+					if (chrome.runtime.lastError) {
+						alert("Попробуйте перезагрузить страницу!");
+					}
+					else {
+						alert(res.isTestTab);
+						var hiddenElement = document.createElement('a');
+						hiddenElement.href = 'data:attachment/text,' + encodeURI(JSON.stringify(res));
+						hiddenElement.target = '_blank';
+						hiddenElement.download = 'Test.json';
+						hiddenElement.click();
+					}
 				});
-			}
-			catch(e){
-				if (e instanceof runtime.lastError) {
-					alert("Попробуйте перезагрузить страницу!")
-				}
-				else {
-					alert("Непридвиденная ошибка!");
-					alert(e);
-				}
-			}
+			});
 		});
 	}
 }
